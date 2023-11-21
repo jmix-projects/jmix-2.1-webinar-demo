85259b456efce934fa29efeca499deb6ceeb4d4b
Konstantin Krivopustov <krivopustov@haulmont.com>
2023-11-21 09:35:13 +0400

    Prep to demo

--- src/main/java/com/company/onboarding/view/book/BookDetailView.java
+++ src/main/java/com/company/onboarding/view/book/BookDetailView.java
@@ -1,14 +1,9 @@
 package com.company.onboarding.view.book;
 
 import com.company.onboarding.entity.Book;
-
 import com.company.onboarding.view.main.MainView;
-
 import com.vaadin.flow.router.Route;
-import io.jmix.core.DataManager;
-import io.jmix.flowui.component.textfield.TypedTextField;
 import io.jmix.flowui.view.*;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "books/:id", layout = MainView.class)
 @ViewController("Book.detail")
